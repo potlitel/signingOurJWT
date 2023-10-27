004b987eca2fbe3b290b89750a6702f2e3c499bf
potlitel <potlitel@gmail.com>
2023-10-27 14:04:57 -0400

    Add project files.

---
+++ signingOurJWT/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using signingOurJWT.Samples;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // HMAC using SHA-256
+        HmacExample.Run();
+
+        // RSASSA-PSS using SHA-256 and MGF1 with SHA-256
+        //RsaExample.Run();
+
+        // ECDSA using P-256 and SHA-256
+        //ECDsaExample.Run();
+    }
+}
